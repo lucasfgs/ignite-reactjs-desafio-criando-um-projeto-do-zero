c07701dc8fb19d6bc20529e84716860528f6baf1
Lucas Ferreira <lucsferreira.dev@gmail.com>
2021-08-08 18:08:56 -0400

    Fetching data and rendering on slug page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <Link href="/">
       <header className={styles.headerContainer}>
-        <img src="/images/logo.svg" alt="spacetraveling" />
+        <img src="/images/logo.svg" alt="logo" />
       </header>
     </Link>
   );
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,13 @@
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { GetStaticPaths, GetStaticPropsContext, GetStaticProps } from 'next';
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import Prismic from '@prismicio/client';
 import { BiTime } from 'react-icons/bi';
-import { IoMdTime } from 'react-icons/io';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
+import { useEffect } from 'react';
+import { RichText } from 'prismic-dom';
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
 
 import Header from '../../components/Header';
@@ -14,6 +18,7 @@ import styles from './post.module.scss';
 interface Post {
   first_publication_date: string | null;
   data: {
+    reading_time: string;
     title: string;
     banner: {
       url: string;
@@ -32,7 +37,10 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
+
+  if (router.isFallback) return <div>Carregando...</div>;
   return (
     <main>
       <Header />
@@ -40,75 +48,90 @@ export default function Post() {
       <div className={commonStyles.contentContainer}>
         <article className={styles.postContent}>
           <header>
-            <h2>Criando um APP CRA do zero</h2>
+            <h2>{post.data.title}</h2>
             <div className={styles.postInfo}>
               <p>
                 <AiOutlineCalendar />
-                15 Mar 2021
+                {post.first_publication_date}
               </p>
               <p>
                 <AiOutlineUser />
-                Joseph Oliveira
+                {post.data.author}
               </p>
               <p>
-                <BiTime />4 min
+                <BiTime />
+                {post.data.reading_time} min
               </p>
             </div>
           </header>
-          <section className={styles.postArcticle}>
-            <h3>Porin et varius</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-              incidunt dolores doloribus at quae nesciunt quaerat dolore
-              repudiandae eos consequatur esse ipsa sint animi, numquam quas
-              itaque hic nihil! At?
-            </p>
-          </section>
-          <section className={styles.postArcticle}>
-            <h3>Porin et varius</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-              incidunt dolores doloribus at quae nesciunt quaerat dolore
-              repudiandae eos consequatur esse ipsa sint animi, numquam quas
-              itaque hic nihil! At?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-              incidunt dolores doloribus at quae nesciunt quaerat dolore
-              repudiandae eos consequatur esse ipsa sint animi, numquam quas
-              itaque hic nihil! At?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-              incidunt dolores doloribus at quae nesciunt quaerat dolore
-              repudiandae eos consequatur esse ipsa sint animi, numquam quas
-              itaque hic nihil! At?
-            </p>
-          </section>
+          {post.data.content.map(content => (
+            <section className={styles.postArcticle} key={content.heading}>
+              <h3>{content.heading}</h3>
+              {content.body.map(body => (
+                <p key={body.text}>{body.text}</p>
+              ))}
+            </section>
+          ))}
         </article>
       </div>
     </main>
   );
 }
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
   const posts = await prismic.query([
     Prismic.predicates.at('document.type', 'post'),
   ]);
 
-  const paths = posts.results.map(post => ({
-    params: { id: post.uid.toString() },
-  }));
-
-  console.log(paths);
+  const paths = posts.results.map(post => {
+    return {
+      params: { slug: post.uid.toString() },
+    };
+  });
 
   return { paths, fallback: true };
 };
 
-export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
-  // const prismic = getPrismicClient();
-  // const response = await prismic.getByUID(TODO);
-  // console.log(params);
-  // TODO
+interface GetStaticPropsParams {
+  params: { slug: string };
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: GetStaticPropsParams) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('post', slug, null);
+
+  const amounOfWordsHeading = response.data.content.reduce(
+    (acc, currentValue) => {
+      if (currentValue.heading) {
+        return [...acc, ...currentValue.heading.split(' ')];
+      }
+
+      return [...acc];
+    },
+    []
+  ).length;
+
+  const amountOfWordsOfBody = RichText.asText(
+    response.data.content.reduce((acc, data) => [...acc, ...data.body], [])
+  ).split(' ').length;
+
+  response.data.reading_time = Math.ceil(
+    (amounOfWordsHeading + amountOfWordsOfBody) / 200
+  );
+
+  response.first_publication_date = format(
+    parseISO(response.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
+  console.log(response.first_publication_date);
+
+  return { props: { post: response } };
 };
